0921e2d9eb41dea472747a56c6e2f3f705248cb8
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-11-01 15:12:49 +0500

    create signupuser method

--- src/pages/auth/signUp.js
+++ src/pages/auth/signUp.js
@@ -1,10 +1,25 @@
 import React from 'react'
 import Form from '../../components/form'
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from '../../firebase/conFig';
 
 const SignUp = () => {
+  const signUpUser = ( email, password) => {
+    createUserWithEmailAndPassword(auth,email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+     console.log(user)
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+    console.log(errorMessage);
+    });
+  }
+
   return (
     <div>
-      <Form signUp ></Form>
+      <Form signUp  signUpUser = {signUpUser} ></Form>
     </div>
   )
 }
