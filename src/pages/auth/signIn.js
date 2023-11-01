4e99450500aab232215fa159684defa21a7938d2
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-11-01 15:13:05 +0500

    create signin method

--- src/pages/auth/signIn.js
+++ src/pages/auth/signIn.js
@@ -1,10 +1,23 @@
 import React from 'react'
 import Form from '../../components/form'
-
+import {signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from '../../firebase/conFig'
 const SignIn = () => {
+  const signInUser = (email, password) => {
+    signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+     console.log(user)
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+    console.log(errorMessage);
+    });
+  }
   return (
     <div>
-        <Form></Form>
+        <Form signInUser={signInUser}></Form>
     </div>
   )
 }
