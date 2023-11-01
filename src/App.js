30f7784a2e6a934e7d40460f9ae351b335b657b8
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-11-01 10:17:37 +0500

    define routes for elements

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/navbar';
 import Home from './pages/home';
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
+     <Routes>
+      <Route path = '/' element={<Navbar />}>
+      <Route index element={<Home />} />
+      </Route>
+     </Routes>
     </div>
   );
 }
