9cf3d8b68eb7c06750fb6d5a89ddbcbdeac41f6f
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-11-02 10:13:09 +0500

    login user allow

--- src/pages/auth/signIn.js
+++ src/pages/auth/signIn.js
@@ -8,49 +8,51 @@ const SignIn = () => {
     signInWithEmailAndPassword(auth, email, password)
       .then(async (userCredential) => {
         const user = userCredential.user;
-        // FIRST WAY
-        // onValue(ref(database, `users/${user.uid}`), (data) => {
-        //   console.log("data====>", data.val());
-        // const data = data.val();
-        // })
-
+        if(user){
+         alert("SignIn sucessfull")
+         
+          // FIRST WAY
+          // onValue(ref(database, `users/${user.uid}`), (data) => {
+            //   console.log("data====>", data.val());
+            // const data = data.val();
+            // })
+            
         // SECOND WAY
         //   const userData = ref(database, `users/${user.uid}`);
         // onValue(userData, (snapshot) => {
-        //   const data = snapshot.val();
+          //   const data = snapshot.val();
         //   console.log(data)
         // });
-
+        
         //  FIRST WAY OF  (GET METHOD)
         // get(ref(dbRealTime,`users/${user.uid}`))
         // .then((data) => {
-        //   if (data.exists()) {
-        //     alert("User Sucessfully login")
-        //     // console.log(data.val());
-        //   } else {
+          //   if (data.exists()) {
+            //     alert("User Sucessfully login")
+            //     // console.log(data.val());
+            //   } else {
         //     console.log("No data available");
         //   }
         // })
         // .catch((error) => {
-        //   console.error(error);
-        // });
-
-        //  SECONDE WAY OF  (GET METHOD)
-        // const dbRef = ref(database);
-        // get(child(dbRef, `users/${user.uid}`))
-        //   .then((data) => {
-        //     if (data.exists()) {
-        //       console.log(data.val());
-        //     } else {
+          //   console.error(error);
+          // });
+          
+          //  SECONDE WAY OF  (GET METHOD)
+          // const dbRef = ref(database);
+          // get(child(dbRef, `users/${user.uid}`))
+          //   .then((data) => {
+            //     if (data.exists()) {
+              //       console.log(data.val());
+              //     } else {
         //       console.log("No data available");
         //     }
         //   })
         //   .catch((error) => {
         //     console.error(error);
         //   });
-
+        
         // FIRSTORE GET DATA
-
         const userRef = doc(dbFireStore, "users", user.uid);
         const userData = await getDoc(userRef);
         if (userData.exists()) {
@@ -58,7 +60,9 @@ const SignIn = () => {
         } else {
           console.log("No such document!");
         }
+      }
       })
+
       .catch((error) => {
         const errorCode = error.code;
         const errorMessage = error.message;
