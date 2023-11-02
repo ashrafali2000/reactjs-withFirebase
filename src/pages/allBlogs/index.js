b174be100e8ede7e3e49b5033457cd6c21676a68
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-11-02 13:12:12 +0500

    get all user blogs

--- src/pages/allBlogs/index.js
+++ src/pages/allBlogs/index.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import { doc, getDoc, getDocs, dbFireStore, where, collection, query } from "../../firebase/conFig";
 
 const AllBlogs = () => {
-  return (
-    <div>AllBlogs</div>
-  )
-}
+  (async function foo() {
 
-export default AllBlogs
+    const q = query(collection(dbFireStore, "users"));
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      // doc.data() is never undefined for query doc snapshots
+      const userBlogs = doc.data().blogs;
+      console.log(" =====> ", userBlogs);
+    });
+  })();
+
+  return (
+    <div>
+      <div>AllBlogs</div>
+      <div></div>
+    </div>
+  );
+};
+
+export default AllBlogs;
